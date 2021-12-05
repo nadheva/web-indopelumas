b18ad36ad2340135fa1fa105719d3939d6656848
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-12-05 22:01:30 +0700

    update migration

--- database/migrations/2021_12_05_075325_create_produk_table.php
+++ database/migrations/2021_12_05_075325_create_produk_table.php
@@ -18,6 +18,7 @@ class CreateProdukTable extends Migration
             $table->string('produk');
             $table->string('foto');
             $table->text('deskripsi');
+            $table->string('slug');
             $table->timestamps();
         });
     }
