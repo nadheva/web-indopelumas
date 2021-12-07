b85e3ace77bd615cdfc33773b6124bc8272595aa
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-12-07 16:33:09 +0700

    Setting Auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             ProdukSeeder::class,
             TestimonialsSeeder::class,
             IsiProdukSeeder::class,
--- routes/web.php
+++ routes/web.php
@@ -21,51 +21,49 @@ use App\Http\Controllers\LandingPageController;
 
 Route::get('', [LandingPageController::class, 'home'])->name('landingpage');
 Route::get('artikel', [LandingPageController::class, 'artikel'])->name('artikel');
-
-
-
-
 Route::get('/about-us', function () {
     return view('users.about-us');
 })->name('aboutus');
-
 Route::get('/products', function () {
     return view('users.products');
 });
-
 Route::get('/contact-us', function () {
     return view('users.contact-us');
 })->name('contactus');
-
 Route::get('product/{id}', [LandingPageController::class, 'details'])->name('details');
 
 
 //Admin
-Route::get('/admin', function () {
-    return view('admin.index');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/admin', function () {
+        return view('admin.index');
+    });
+    Route::resource('produk', ProdukController::class);
+    Route::resource('isiproduk', IsiProdukController::class);
+
+    // Isi Produk 
+    Route::get('tambah-isi-produk/{id}', [IsiProdukController::class, 'create'])->name('tambahisiproduk');
+    Route::get('edit-isi-produk/{id}', [IsiProdukController::class, 'edit']);
+    Route::put('update-isi-produk/{id}', [IsiProdukController::class, 'update'])->name('updateisiproduk');
+    Route::get('delete-isi-produk/{id}', [IsiProdukController::class, 'delete'])->name('deleteisiproduk');
+
+    Route::get('produk/delete/{id}', [ProdukController::class, 'delete']);
+
+    Route::get('product-details/{id}', [IsiProdukController::class, 'productDetails'])->name('showdata');
+
+    Route::resource('news', NewsController::class);
+    Route::get('news/delete/{id}', [NewsController::class, 'delete']);
+    Route::get('news/show/{id}', [NewsController::class, 'show']);
+    Route::resource('komentar', KomentarController::class);
+    Route::resource('testimonials', TestimonialsController::class);
+    Route::get('testimonials/delete/{id}', [testimonialsController::class, 'delete']);
+
+    Route::get('/dashboard', function () {
+        return view('admin.produk.index');
+    })->middleware(['auth'])->name('dashboard');
 });
-Route::resource('produk', ProdukController::class);
-Route::resource('isiproduk', IsiProdukController::class);
 
-// Isi Produk 
-Route::get('tambah-isi-produk/{id}', [IsiProdukController::class, 'create'])->name('tambahisiproduk');
-Route::get('edit-isi-produk/{id}', [IsiProdukController::class, 'edit']);
-Route::put('update-isi-produk/{id}', [IsiProdukController::class, 'update'])->name('updateisiproduk');
-Route::get('delete-isi-produk/{id}', [IsiProdukController::class, 'delete'])->name('deleteisiproduk');
+require __DIR__ . '/auth.php';
 
-Route::get('produk/delete/{id}', [ProdukController::class, 'delete']);
 
-Route::get('product-details/{id}', [IsiProdukController::class, 'productDetails'])->name('showdata');
-
-Route::resource('news', NewsController::class);
-Route::get('news/delete/{id}', [NewsController::class, 'delete']);
-Route::get('news/show/{id}', [NewsController::class, 'show']);
-Route::resource('komentar', KomentarController::class);
-Route::resource('testimonials', TestimonialsController::class);
-Route::get('testimonials/delete/{id}', [testimonialsController::class, 'delete']);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
