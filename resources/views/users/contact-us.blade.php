9862097d7ad7483a4c11cf7aa308024c133eb673
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-12-07 23:07:57 +0700

    test

--- resources/views/users/contact-us.blade.php
+++ resources/views/users/contact-us.blade.php
@@ -65,9 +65,9 @@
               <textarea class="form-control" name="pesan" rows="5" placeholder="Message" required></textarea>
             </div>
             <div class="my-3">
-              <div class="loading">Loading</div>
-              <div class="error-message"></div>
-              <div class="sent-message">Your message has been sent. Thank you!</div>
+              {{-- <div class="loading">Loading</div> --}}
+              {{-- <div class="error-message"></div> --}}
+              {{-- <div class="sent-message">Your message has been sent. Thank you!</div> --}}
             </div>
             <div class="text-center"><button type="submit">Send Message</button></div>
           </form>
